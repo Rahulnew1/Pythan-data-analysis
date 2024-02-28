fb18dc35da8cb8e4112278792f4cd0586b129d28
rahul prakash <win.rahul55@yahoo.com>
2024-02-28 19:48:28 +0530

    game game

--- game2.py
+++ game2.py
@@ -1,22 +1,63 @@
 import pgzrun
 import random
 
-class Hero(Actor):
-    def move(self):
-        if keybord.left:
-            self.x-=5
-        elif keybord
-
 HIGHT=500
 WIDTH=800
+
+class Hero(Actor):
+    def move(self):
+        if keyboard.left:
+            self.x-=5
+        elif keyboard.right:
+            self.x+=5
+        elif keyboard.up:
+            self.y-=5
+        elif keyboard.down:
+            self.y+=5
+        else:
+            self.angle=0
+
+    def warp(self):
+        if self.x> WIDTH:
+            self.x =0
+        if self.x<0:
+            self.x=WIDTH
+        if self.y> HIGHT:
+            self.Y =0
+        if self.Y<0:
+            self.Y=HIGHT
+        
+
+class duck(Actor):
+    def track(self,player,speed=1):
+        if player.x > self.x:
+            self.x += speed
+        elif player.x < self.x:
+            self.x -= speed
+        if player.y> self.y:
+            self.y += speed
+        elif player.y <self.y:
+            self.y-= speed
+
+
+
 p=Hero('mario',(100,100))
-b=Rect((300,100),50,50)
+b=Rect((300,100),(50,50))
+s=duck('duck',(1000.1000))
+e=sneak('sneak',(300,1000))
+
 def draw():
     screen.fill('white')
     p.draw()
-    screen.drow.filled_rect(b,'blue')
+    s.draw()
+    e.deaw()
+    screen.draw.filled_rect(b,'blue')
+
 def update():
-    pass
+    p.move()
+    p.move()
+    s.track(P,4)
+    e.track(p,1)
 
 pgzrun.go()
 
